12911df7cb3663e1f7c1eb91976a4d4751f9c89a
Enechukwu Chibuike <enechukwuchibuike0@gmail.com>
2024-05-07 12:36:29 +0100

    added events branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
+import { useState } from "react";
 import "./App.css";
+import Increment from "./components/increment";
+import Bulb from "./components/Bulb";
 
 function App() {
+  const [text, setText] = useState("hiii");
+
+  const handleClick = () => {
+    if (text === "hello") {
+      setText("hiii");
+    } else {
+      setText("hello");
+    }
+  };
+
   return (
-    <main className="bg-gray-700">
-      <h1>Events</h1>
-    </main>
+    <>
+      <main className="text-xl h-screen flex justify-center items-center flex-col w-screen">
+        <h1>{text}</h1>
+        <button className="bg-blue-500 p-2" onClick={handleClick}>
+          Click me!
+        </button>
+      </main>
+      <Increment />
+      <Bulb />
+    </>
   );
 }
 
